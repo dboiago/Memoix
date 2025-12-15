ca6d1df86573c23dab62417a5b75be25d060fe68
dboiago <dboiago@gmail.com>
2025-12-15 12:51:14 -0500

    Move recipe_parser_strategy.dart to correct directory

--- lib/core/services/importer/recipe_parser_strategy.dart
+++ lib/core/services/importer/recipe_parser_strategy.dart
@@ -1,9 +1,8 @@
 import 'package:html/dom.dart';
 import '../../models/recipe_import_result.dart';
 
-/// Strategy interface for parsing recipes from different sources
 abstract class RecipeParserStrategy {
-  /// Returns a confidence score (0.0 - 1.0) indicating if this strategy can handle the URL/Content
+  /// Returns a confidence score (0.0 - 1.0)
   double canParse(String url, Document? document, String? rawBody);
 
   /// Executes the parsing logic
