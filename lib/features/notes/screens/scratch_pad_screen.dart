69b568b65d6c39fbfae991df38a4254047b53073
dboiago <dboiago@gmail.com>
2026-01-09 16:27:27 -0500

    fix

--- lib/features/notes/screens/scratch_pad_screen.dart
+++ lib/features/notes/screens/scratch_pad_screen.dart
@@ -152,22 +152,12 @@ class _ScratchPadScreenState extends ConsumerState<ScratchPadScreen>
     Navigator.push(
       context,
       MaterialPageRoute(
-        // Use the new external screen
         builder: (_) => DraftEditorScreen(
           initialDraft: draft,
         ),
       ),
-    ).then((_) => ref.refresh(recipeDraftsProvider)); 
+    ).then((_) => ref.refresh(recipeDraftsProvider));
   }
-  
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (_) => DraftEditorScreen(
-            initialDraft: draft,
-          ),
-        ),
-      ).then((_) => ref.refresh(recipeDraftsProvider)); // Refresh list on return
 
   Future<void> _deleteDraft(WidgetRef ref, RecipeDraft draft) async {
     await ref.read(scratchPadRepositoryProvider).deleteDraft(draft.uuid);
