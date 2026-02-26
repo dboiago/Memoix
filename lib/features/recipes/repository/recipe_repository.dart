4ac047db23f64bf5c321e7858c893551b5674401
dboiago <dboiago@gmail.com>
2026-02-26 11:38:03 -0500

    variable name fix

--- lib/features/recipes/repository/recipe_repository.dart
+++ lib/features/recipes/repository/recipe_repository.dart
@@ -207,7 +207,7 @@ class RecipeRepository {
     await IntegrityService.reportEvent(
       'activity.recipe_favourited',
       metadata: {
-        'recipe_id': recipe.uuid,
+        'recipe_id': existing.uuid,
         'is_adding': !wasFavorited,
       },
     );
