2e42dab1fcbb063032577d8906e05e18c5aeb401
dboiago <dboiago@gmail.com>
2026-03-27 15:52:45 -0400

    Temp upload generated files for migration made easier I hope

---
+++ lib/data/drift/daos/meal_plan_dao.g.dart
@@ -0,0 +1,9 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'meal_plan_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$MealPlanDaoMixin on DatabaseAccessor<AppDatabase> {
+  $MealPlansTable get mealPlans => attachedDatabase.mealPlans;
+  $PlannedMealsTable get plannedMeals => attachedDatabase.plannedMeals;
+}
