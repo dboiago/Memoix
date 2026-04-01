a2b42d26734fe1f4dc211c97eedde2865bcff6ae
dboiago <dboiago@gmail.com>
2026-04-01 10:13:45 -0400

    Missing import

--- lib/features/personal_storage/screens/personal_storage_screen.dart
+++ lib/features/personal_storage/screens/personal_storage_screen.dart
@@ -11,6 +11,7 @@ import '../models/sync_mode.dart';
 import '../models/sync_status.dart';
 import '../providers/google_drive_storage.dart';
 import '../providers/one_drive_storage.dart';
+import '../providers/personal_storage_provider.dart';
 import '../services/personal_storage_service.dart';
 import '../services/shared_storage_manager.dart';
 import '../services/storage_provider_manager.dart';
