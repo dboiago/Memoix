17e747e41b43b50889317a5879e197b3e4ea2532
Devon Boiago <dboiago@gmail.com>
2026-04-24 22:49:51 -0400

    updating cause annoying

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,19 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystorePropertiesFile = rootProject.file("key.properties")
+val keystoreProperties = Properties()
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+} else {
+    // This forces the build to loudly crash if it can't find your file!
+    throw GradleException("Could not find key.properties file! Looking in: ${keystorePropertiesFile.absolutePath}")
 }
 
 android {
@@ -13,17 +24,27 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-		isCoreLibraryDesugaringEnabled = true
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
+    // ADDED: Your Release Signing Configuration
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "io.github.dboiago.memoix"
-		manifestPlaceholders["appAuthRedirectScheme"] = "io.github.dboiago.memoix"
+        manifestPlaceholders["appAuthRedirectScheme"] = "io.github.dboiago.memoix"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -32,11 +53,10 @@ android {
         versionName = flutter.versionName
     }
 
-	buildTypes {
+    buildTypes {
         getByName("release") {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // CHANGED: Now points to the release block above instead of "debug"
+            signingConfig = signingConfigs.getByName("release")
             
             // Make sure this line exists with the parentheses and double quotes!
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
