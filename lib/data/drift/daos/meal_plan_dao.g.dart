4a883ba4cdadc453c8670c633ae6141423cc844b
Devon Boiago <dboiago@gmail.com>
2026-05-07 09:33:49 -0400

    Delete lib/data/drift/daos/meal_plan_dao.g.dart

--- lib/data/drift/daos/meal_plan_dao.g.dart
+++
@@ -1,9 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'meal_plan_dao.dart';
-
-// ignore_for_file: type=lint
-mixin _$MealPlanDaoMixin on DatabaseAccessor<AppDatabase> {
-  $MealPlansTable get mealPlans => attachedDatabase.mealPlans;
-  $PlannedMealsTable get plannedMeals => attachedDatabase.plannedMeals;
-}
