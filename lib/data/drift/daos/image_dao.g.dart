b475bf94c15bb2cf48b6d5b5508c01a121e4f772
Devon Boiago <dboiago@gmail.com>
2026-05-07 09:33:39 -0400

    Delete lib/data/drift/daos/image_dao.g.dart

--- lib/data/drift/daos/image_dao.g.dart
+++
@@ -1,9 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'image_dao.dart';
-
-// ignore_for_file: type=lint
-mixin _$ImageDaoMixin on DatabaseAccessor<AppDatabase> {
-  $RecipesTable get recipes => attachedDatabase.recipes;
-  $RecipeImagesTable get recipeImages => attachedDatabase.recipeImages;
-}
