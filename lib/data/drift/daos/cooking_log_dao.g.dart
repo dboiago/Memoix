8748ff4255da97724a52089bb6ae346e126b3f7f
Devon Boiago <dboiago@gmail.com>
2026-05-07 09:31:41 -0400

    Delete lib/data/drift/daos/cooking_log_dao.g.dart

--- lib/data/drift/daos/cooking_log_dao.g.dart
+++
@@ -1,8 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'cooking_log_dao.dart';
-
-// ignore_for_file: type=lint
-mixin _$CookingLogDaoMixin on DatabaseAccessor<AppDatabase> {
-  $CookingLogsTable get cookingLogs => attachedDatabase.cookingLogs;
-}
